b2b2cec4448b54123d6e015d5850aaaba930c5b0
HasinduWelarathne <hasinduwelarathne@gmail.com>
2024-08-21 02:10:36 +1000

    Update config.py to use DATABASE_URL from environment variables

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Settings:
-    DATABASE_URL: str = os.getenv("postgresql://admin:KJXkT4wBUL84SvZuJeoBis71ARzEnzfR@dpg-cr2bdb5svqrc73cmec3g-a.oregon-postgres.render.com/task1_4a8q")
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
 
 settings = Settings()
